fd0079aaeaf1fc4cca02ddd731fe823b2cff91e1
amaniesseili <esseiliamani@hotmail.com>
2023-06-29 20:58:32 +0200

    js  password

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = prompt("inserici il tuo nome");
+const lasttName = prompt("inserici il tuo cognome");
+const favoriteColor = prompt("inserici il tuo colore preferito");
+
+
+document.getElementById("welcome-title").innerHTML = ` Benvenuto ${firstName} ${lasttName}`;
+document.getElementById("color").innerHTML = `${favoriteColor}`;
+document.getElementById("pass").innerHTML = `${firstName}${lasttName}`;
